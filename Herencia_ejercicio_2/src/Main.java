eee57b722efd0d41552d14f47ab8ab822c988057
Cesar M <cesarmunozrj45@gmail.com>
2023-09-04 17:49:00 -0600

    Herencia - ejercicio 3

--- Herencia_ejercicio_2/src/Main.java
+++ Herencia_ejercicio_2/src/Main.java
@@ -3,6 +3,8 @@ import entidad.Lavadora;
 import entidad.Television;
 import servicio.ElectrodomesticoService;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         //ejercicio 3
@@ -14,9 +16,22 @@ public class Main {
 //        lavadora1.crearLavadora();
 //        System.out.println(lavadora1.toString());
 
-        Television tele1 = new Television();
-        tele1.crearTelevisor();
-        System.out.println(tele1.toString());
+//        Television tele1 = new Television();
+//        tele1.crearTelevisor();
+//        System.out.println(tele1.toString());
+
+        ArrayList<Electrodomestico> listaElectro = new ArrayList<>();
+        listaElectro.add(new Electrodomestico(0, "AZUL", 'F', 25));
+        listaElectro.add(new Lavadora(0, "ROJO", 'F', 50,500));
+        listaElectro.add(new Television(0, "NEGRO", 'A', 25, 50, true));
+
+        double total = 0;
+        for (Electrodomestico electro: listaElectro) {
+            electro.precioFinal();
+            System.out.println(electro.getPrecio());
+            total += electro.getPrecio();
+        }
+        System.out.println("Precio total de la Lista: " + total);
 
     }
 }
