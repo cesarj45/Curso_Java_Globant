0c3f2bc7d9c07173672754ea5585fc1436c86a78
Cesar M <cesarmunozrj45@gmail.com>
2023-10-12 18:15:48 -0600

    Driver JDBC

--- Herencia_ejercicio_4/src/entidad/CalculosFormas.java
+++ Herencia_ejercicio_4/src/entidad/CalculosFormas.java
@@ -2,6 +2,6 @@ package entidad;
 
 public interface CalculosFormas {
     public static final double PI = 3.1416;
-    public double calcularArea();
+    public  double calcularArea();
     public double calcularPerimetro();
 }
